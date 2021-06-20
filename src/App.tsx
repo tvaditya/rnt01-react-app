17661105af72bca08b2d335c355900c00be30a88
tvaditya <tvaditya@gmail.com>
2021-06-20 02:40:09 -0300

    Register component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from "./components/Nav";
-import Menu from "./components/Menu"
+import {BrowserRouter, Route } from 'react-router-dom';
+import Users from './pages/Users';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route path={'/'} exact component={Users}/>
+        <Route path={'/login'} component={Login}/>
+        <Route path={'/register'} component={Register}/>
+      </BrowserRouter>
     </div>
   );
 }

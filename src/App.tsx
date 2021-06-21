14389f606da038cc70030f1fc854a6f68445e0b8
tvaditya <tvaditya@gmail.com>
2021-06-21 00:45:10 -0300

    User page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {BrowserRouter, Route } from 'react-router-dom';
 import Users from './pages/Users';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {RedirectUsers} from "./components/RedirectToUsers";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path={'/'} exact component={Users}/>
+        <Route path={'/'} exact component={RedirectUsers} />
+        <Route path={'/'} component={Users}/>
         <Route path={'/login'} component={Login}/>
         <Route path={'/register'} component={Register}/>
       </BrowserRouter>
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,27 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 import Layout from "../components/Layout";
+import { User } from "../models/users";
+
 
 const Users = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+(        async () => {
+            const {data} = await axios.get('ambassador');
+
+            setUsers(data);
+        })()
+    }, [])
     return (
         <Layout>
             <table className="table table-striped table-sm">
                 <thead>
                 <tr>
                     <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                </tr>
+                {users.map(user => {
+                    return (
+                        <tr>
+                            <td>{user.id}</td>
+                            <td>{user.first_name} {user.last_name}</td>
+                            <td>{user.email}</td>
+                            <td></td>
+                        </tr>
+                    );
+                })};
+
                 </tbody>
             </table>
         </Layout>
